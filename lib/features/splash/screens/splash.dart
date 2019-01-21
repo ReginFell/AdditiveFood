80da1ca3e9a5c129e26d3dcc2079e55eb1bcf212
ReginFell <ReginFell@gmail.com>
2019-01-21 21:06:46 +0200

    Add pubspec

--- lib/features/splash/screens/splash.dart
+++ lib/features/splash/screens/splash.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   static final String route = '/';
+
+  SplashScreen() : super(key: Key(route));
+
+  @override
+  SplashScreenState createState() {
+    return new SplashScreenState();
+  }
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
+    return Image(
+      image: AssetImage("assets/images/background.png"),
+      fit: BoxFit.fitWidth,
     );
   }
 }
